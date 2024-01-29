f1bfdd5f5bb113561d492354b89c48952db495b4
martin-creator <martinlubowa@outlook.com>
2024-01-29 23:34:12 +0300

    Updated rating model with foreign relationships

--- recommender_system/movies/models.py
+++ recommender_system/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
 
@@ -7,4 +8,5 @@ class Movie(models.Model):
     overview = models.TextField()
     release_date = models.DateField(blank=True, null=True, auto_now=False, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+    # rating = GenericRelation(Rating, related_query_name='movies')
 
--- recommender_system/ratings/models.py
+++ recommender_system/ratings/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 # Create your models here.
+
+
+User = settings .AUTH_USER_MODEL # get the currently active user model,
+
+class RatingChoice(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    __empty__ = 'Rate this movie'
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField(null=True, blank=True, choices=RatingChoice.choices)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = models.GenericForeignKey("content_type", "object_id")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} rated {self.movie.title} {self.stars} stars'
