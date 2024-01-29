6af85ea7b15e128bda81c03fe91f781a5f90eec9
martin-creator <martinlubowa@outlook.com>
2024-01-29 18:36:23 +0300

    Added args for movies and users on manage.py

--- recommender_system/profiles/management/commands/loader.py
+++ recommender_system/profiles/management/commands/loader.py
@@ -12,19 +12,29 @@ class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         '''Add arguments to the command'''
         parser.add_argument('count', type=int, default=10, nargs='?')
+        parser.add_argument('--movies', action='store_true', default=False)
+        parser.add_argument('--users', action='store_true', default=False)
         parser.add_argument('--show-total', action='store_true', default=False)
 
     def handle(self, *args: Any, **options: Any) -> str | None:
        '''Handle the command'''
        count = options['count']
        show_total = options['show_total']
-       profiles = rs_utils.get_fake_profiles(count=count)
-       new_users = []
-       for profile in profiles:
-           new_users.append(User(**profile))
-       user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
-       self.stdout.write(self.style.SUCCESS(f'Created {len(user_bulk)} users'))
-       if show_total:
-              self.stdout.write(self.style.SUCCESS(f'Total users: {User.objects.count()}'))
+       load_movies = options['movies']
+       generate_users = options['users']
+       
+       if load_movies:
+           rs_utils.load_movie_data(limit=count)
+           return self.stdout.write(self.style.SUCCESS(f'Loaded {count} movies'))
+       
+       if generate_users:
+            profiles = rs_utils.get_fake_profiles(count=count)
+            new_users = []
+            for profile in profiles:
+                new_users.append(User(**profile))
+            user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+            self.stdout.write(self.style.SUCCESS(f'Created {len(user_bulk)} users'))
+            if show_total:
+                    self.stdout.write(self.style.SUCCESS(f'Total users: {User.objects.count()}'))
 
         
