59e2b7a6f85c41cfb8cd0dd54267274b33b77065
martin-creator <martinlubowa@outlook.com>
2024-02-04 11:15:54 +0300

    Added tasks for movies

--- recommender_system/movies/tasks.py
+++ recommender_system/movies/tasks.py
@@ -1,20 +1,24 @@
 from celery import shared_task
 
-from .models import Movie
+from django.apps import apps
 
-@shared_task(name='task_calculate_movie_ratings')
-def task_calculate_movie_ratings(all=False, count=None):
-    '''
-    task_calculate_movie_ratings(all=False, count=None)
-    # celery tasks
-    task_calculate_movie_ratings.delay(all=False, count=None)
-    task_calculate_movie_ratings.apply_async(kwargs={"all": False, "count": 12}, countdown=30)
-    '''
-    qs = Movie.objects.needs_updating()
-    if all:
-        qs = Movie.objects.all()
-    qs = qs.order_by('rating_last_updated')
-    if isinstance(count, int):
-        qs = qs[:count]
+from django.db.models import Window, F
+from django.db.models.functions import DenseRank
+
+
+@shared_task
+def update_movie_position_embedding_idx():
+    Movie = apps.get_model('movies', "Movie")
+    qs = Movie.objects.all().annotate(
+        new_idx=Window(
+            expression=DenseRank(),
+            order_by=[F('id').asc()]
+        )
+    ).annotate(final_idx = F('new_idx') - 1)
+    updated = 0
     for obj in qs:
-        obj.calculate_rating(save=True)
+        if obj.final_idx != obj.idx:
+            updated += 1
+            obj.idx = obj.final_idx
+            obj.save()
+    print(f"Updated {updated} movie idx fields")
