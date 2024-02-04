7160bf3ec3d5cbd48e50c5653d985a0730e897a5
martin-creator <martinlubowa@outlook.com>
2024-02-04 11:16:23 +0300

    Updated movies views

--- recommender_system/movies/views.py
+++ recommender_system/movies/views.py
@@ -4,8 +4,10 @@ from .models import Movie
 
 
 SORTING_CHOICES = {
-    "popular": "-rating_avg",
-    "unpopular": "rating_avg",
+    "popular": "popular",
+    "unpopular": "unpopular",
+    "top rated": "-rating_avg",
+    "low rated": "rating_avg",
     "recent": "-release_date",
     "old": "release_date"
 }
@@ -17,11 +19,14 @@ class MovieListView(generic.ListView):
 
     def get_queryset(self):
         request = self.request
-        default_sort = request.session.get('movie_sort_order') or '-rating_avg'
-        qs =  Movie.objects.all().order_by(default_sort)
-        sort = request.GET.get('sort')
+        sort = request.GET.get('sort') or request.session.get('movie_sort_order') or 'popular'
+        qs =  Movie.objects.all()
         if sort is not None:
             request.session['movie_sort_order'] = sort
+            if sort == 'popular':
+                return qs.popular()
+            elif sort == 'unpopular':
+                return qs.popular(reverse=True)
             qs = qs.order_by(sort)
         return qs
 
@@ -64,3 +69,48 @@ class MovieDetailView(generic.DetailView):
 
 
 movie_detail_view = MovieDetailView.as_view()
+
+
+class MovieInfiniteRatingView(MovieDetailView):
+    def get_object(self):
+        user = self.request.user
+        # exclude_ids = []
+        # if user.is_authenticated:
+        #     exclude_ids = [x.object_id for x in user.rating_set.filter(active=True)]
+        return Movie.objects.all().order_by("?").first()
+    
+    def get_template_names(self):
+        request = self.request
+        if request.htmx:
+            return ['movies/snippet/infinite.html']
+        return ['movies/infinite-view.html']
+
+
+movie_infinite_rating_view = MovieInfiniteRatingView.as_view()
+
+
+
+
+class MoviePopularView(MovieDetailView):
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['endless_path'] = '/movies/popular/'
+        return context
+    
+    def get_object(self):
+        user = self.request.user
+        exclude_ids = []
+        if user.is_authenticated:
+            exclude_ids = [x.object_id for x in user.rating_set.filter(active=True)]
+        movie_id_options = Movie.objects.all().popular().exclude(id__in=exclude_ids).values_list('id', flat=True)[:250]
+        return Movie.objects.filter(id__in=movie_id_options).order_by("?").first()
+    
+    def get_template_names(self):
+        request = self.request
+        if request.htmx:
+            return ['movies/snippet/infinite.html']
+        return ['movies/infinite-view.html']
+
+
+movie_popular_view = MoviePopularView.as_view()
